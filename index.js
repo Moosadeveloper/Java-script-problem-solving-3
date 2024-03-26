de789c3988d76c9127178fa9ecc2e33110c29a94
Moosadeveloper <janmoosa77@gmail.com>
2024-03-26 20:19:30 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var productcost = 3.45;
+
+
+var nameofBand;
+
+var productCost = 3.45;
+ var first_Name ="Moosa"
+ var last_Name ="Moosajan"  
+
+
+//  You can't put spaces, you can put underscores instead of spaces For example     var mooas_baloch ="moosa"
+// every veriable are in camlecase.  in camlecase the first letter is lowercase and 2nd veriable with first letter cappile for exaple     var myNumber = 12 ; var moosaJan = 22 ;
+
+
